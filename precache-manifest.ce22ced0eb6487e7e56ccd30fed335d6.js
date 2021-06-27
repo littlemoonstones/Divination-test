22b17017268c68aa7267ff6e639eb706cc63b30f
張育誠 <zhangyucheng@littlemoonstones.local>
2021-06-27 14:08:37 +0800

    depoly

---
+++ precache-manifest.ce22ced0eb6487e7e56ccd30fed335d6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f972d922931a12dd2e15",
+    "url": "/Divination-test/css/about.ce32b64f.css"
+  },
+  {
+    "revision": "350a53f6ad634ee640f7",
+    "url": "/Divination-test/css/app.6c8314f1.css"
+  },
+  {
+    "revision": "8b7ac99b3349dda67192",
+    "url": "/Divination-test/css/chunk-vendors.ff650d32.css"
+  },
+  {
+    "revision": "b6a5762507e790e8aa708c6d90f5bfc9",
+    "url": "/Divination-test/index.html"
+  },
+  {
+    "revision": "f972d922931a12dd2e15",
+    "url": "/Divination-test/js/about.0ebbd462.js"
+  },
+  {
+    "revision": "350a53f6ad634ee640f7",
+    "url": "/Divination-test/js/app.042bdbb9.js"
+  },
+  {
+    "revision": "8b7ac99b3349dda67192",
+    "url": "/Divination-test/js/chunk-vendors.92c10897.js"
+  },
+  {
+    "revision": "706df87e5a9d2b087a03f8fda3f91eed",
+    "url": "/Divination-test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Divination-test/robots.txt"
+  }
+]);
