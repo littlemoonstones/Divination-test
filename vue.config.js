ed153bc187b882e875976d19cdb972f9c7dfb55b
張育誠 <zhangyucheng@littlemoonstones.local>
2021-06-29 01:35:43 +0800

    change title

--- vue.config.js
+++ vue.config.js
@@ -7,5 +7,14 @@ module.exports = {
     workboxOptions: {
       skipWaiting: true
     }
+  },
+  // https://stackoverflow.com/questions/62023604/where-to-find-or-how-to-set-htmlwebpackplugin-options-title-in-project-created-w
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].title = "Divination";
+        return args;
+      })
   }
 }
